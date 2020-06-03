fccf140f157d6be09b3a155231caa91347d848f2
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-06-03 21:17:16 +0300

    initial commit 25-12-2017

---
+++ simpleapp.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import sqlite3 as sql
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
+
+@app.route('/addrecord',methods = ['POST', 'GET'])
+def addrecord():
+   if request.method == 'POST':
+      msg = "Unknown Error"
+      try:
+         name = request.form['name']
+         address = request.form['address']
+         city = request.form['city']
+         zipcode = request.form['zipcode']
+
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,address,city,zipcode) VALUES(?,?,?,?)",(name,address,city,zipcode))
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "Error in insert operation"
+
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+
+   cur = con.cursor()
+   cur.execute("select * from students")
+
+   rows = cur.fetchall();
+   return render_template("list.html",rows = rows)
+
+if __name__ == '__main__':
+   app.run(debug = True)
